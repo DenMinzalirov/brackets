8f439c676f37fd4d9194bcc4500b221dca5afa35
Den Minzakirov <Den.Minzakirov@gmail.com>
2019-03-22 23:15:05 +0300

    inpliment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let tempStr = str;
+  //(2) skan string from the end
+  for (let strKey = tempStr.length; strKey >= 0; strKey--) {
+    // (3) compare with config
+    for (
+      let bracketsKey = 0;
+      bracketsKey < bracketsConfig.length;
+      bracketsKey++
+    ) {
+      if (tempStr[strKey] === bracketsConfig[bracketsKey][0]) {
+        tempStr = tempStr.replace(bracketsConfig[bracketsKey].join(""), "");
+      }
+    }
+  }
+  return tempStr === ""; //(1) true - all pairs removed
+};
